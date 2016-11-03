a081d024365d376800a858add1b66438c7b63cd4
rhamill1 <rhamill11@gmail.com>
2016-11-03 14:26:50 -0700

    adds instrument methods

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,2 +1,12 @@
 class InstrumentsController < ApplicationController
+
+  def index
+    @instruments = Instrument.all
+  end
+
+  def show
+    # explode
+    @instrument = Instrument.find(params[:id])
+  end
+
 end
