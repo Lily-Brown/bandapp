ecd8ed40dfd3d4c2d607f9fbff907e0a24d66466
LilyCole <Lilia.Cole@gmail.com>
2016-11-03 10:41:13 -0700

    adds has_secure_password, adds musicians#new and musicians#create

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,2 +1,24 @@
 class MusiciansController < ApplicationController
+
+  def new
+    @musician = Musician.new
+  end
+
+  def create 
+    @musician = Musician.create(login_musician_params)
+    if @musician.save
+      # flash
+      redirect_to root_path
+    else
+      # flash
+      render :back
+    end
+  end
+
+  private
+
+  def login_musician_params
+    params.require(:musician).permit(:name,:email,:password,:password_confirmation)
+  end
+
 end
