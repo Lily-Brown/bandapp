21bb3d17a3e610aaf2d5637c4ea409b91b1a7555
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-11-04 11:15:08 -0700

    adds edit, update, destroy and auth to band controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,5 +1,6 @@
 class BandsController < ApplicationController
-  before_action :get_band, only: [:show]
+  before_action :get_band, only: [:show, :edit, :update, :destroy]
+  before_action :is_owner, only: [:edit, :update, :destroy]
 
   def index
     @bands = Band.all
@@ -10,7 +11,7 @@ class BandsController < ApplicationController
   end
 
   def create
-    @band = current_musician.bands.create(create_band_params)
+    @band = current_band.bands.create(create_band_params)
     if @band.save
       flash[:success] = 'Band Created!'
       redirect_to @band
@@ -23,6 +24,24 @@ class BandsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @band.update(update_band_params)
+      flash[:success] = 'Band Updated!'
+      redirect_to @band
+    else
+      flash[:error] = @band.errors.full_messages.join(' ')
+      render :edit
+    end
+  end
+
+  def destroy
+    @band.destroy
+    redirect_to root_path
+  end
+
   private
 
   def create_band_params
@@ -32,47 +51,15 @@ class BandsController < ApplicationController
   def get_band
     @band = Band.find(params[:id])
   end
+
+  def update_band_params
+    params.require(:band).permit(:band_name,:genres,:influences,:bio,:zip_code,:image,:audio_clip,:gear)
+  end
+
+  def is_owner
+    if current_musician != @band.musician
+      flash[:error] = 'You do not have permission to perform this action'
+      redirect_to root_path
+    end
+  end
 end
-
-
-
-# ----------
-
-
-#   def show
-#   end
-
-#   def edit
-#   end
-
-#   def update
-#     if @musician.update(update_musician_params)
-#       flash[:success] = 'Musician Updated!'
-#       redirect_to @musician
-#     else
-#       flash[:error] = @musician.errors.full_messages.join(' ')
-#       render :edit
-#     end
-#   end
-
-#   def destroy
-#     @musician.destroy
-#     redirect_to root_path
-#   end
-
-
-
-
-#   def update_musician_params
-#     params.require(:musician).permit(:name,:genres,:influences,:bio,:zip_code,:image,:audio_clip,:gear, instrument_ids:[])
-#   end
-
-
-
-#   def is_current_musician
-#     if current_musician != @musician
-#       flash[:error] = 'You do not have permission to perform this action'
-#       redirect_to current_musician
-#     end
-#   end
-# end
