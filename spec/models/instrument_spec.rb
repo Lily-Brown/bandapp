072725041bfae2358c0cf7440d05a855b3443e16
LilyCole <Lilia.Cole@gmail.com>
2016-11-04 22:43:39 -0700

    adds instrument_specs

--- spec/models/instrument_spec.rb
+++ spec/models/instrument_spec.rb
@@ -2,10 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Instrument, type: :model do
   subject(:instrument) {Instrument.new}
+  subject(:musician) {Musician.new}
 
   describe "::new" do
     it "initializes a new instrument" do
       expect(instrument).to be_a(Instrument)
     end
   end
+
+  describe "::instrument_name" do
+    it "has an name attribute" do
+      instrument.instrument_name = "trumpet"
+      expect(instrument.instrument_name).to eq("trumpet")
+    end
+  end
+
+  describe "::musicians" do
+    it "can belong to a musician" do
+      instrument.musicians << musician
+      expect(instrument.musicians).to eq([musician])
+    end
+    it "can belong to many musicians" do
+      musician2 = Musician.new
+      instrument.musicians << musician2
+      instrument.musicians << musician
+      expect(instrument.musicians).to eq([musician2,musician])
+    end
+  end
+
 end
