08c96eb8e68a0f471026cd3715851640ebfbf53a
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-11-05 13:53:34 -0700

    hot fix, removes avatar validation from Instrument

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,6 +1,5 @@
 class Instrument < ApplicationRecord
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 	# instruments a musician plays
   has_many :instrument_musicians, dependent: :destroy
   has_many :musicians, through: :instrument_musicians
