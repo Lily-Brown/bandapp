7c7fbf4e4f274f10cc21830bee39157738b279e6
rhamill1 <rhamill11@gmail.com>
2016-11-06 14:38:02 -0800

    adds friendly url to bands

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106215814) do
+ActiveRecord::Schema.define(version: 20161106222927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,9 @@ ActiveRecord::Schema.define(version: 20161106215814) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "slug"
     t.index ["musician_id"], name: "index_bands_on_musician_id", using: :btree
+    t.index ["slug"], name: "index_bands_on_slug", unique: true, using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
